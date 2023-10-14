457bc3a93146a378abb46ed378d3d2adb0712654
ashvin.anilkumar <ashvin.anilkumar@qburst.com>
2023-10-14 20:14:31 +0530

    studying...

--- tutorial.js
+++ tutorial.js
@@ -1,4 +1,72 @@
+//! more of arrays
+// let ar = ['blah', 'bleh', 'blooh', 'bluuh'];
+// x = ar.length;
 
+// ar.pop();
+// ar.push('buiih');
+// ar.unshift('buahha');
+// ar.shift();
+// ar.reverse();
+// console.log(ar.includes('blah'))
+// // slice and splice
+// console.log(ar);
+//!DateTimeFormat API
+// let d = new Date();
+// x = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}:${d.getMilliseconds()}
+//  ${d.getDate()} ${d.getFullYear()}`;
+
+// console.log(x);
+// x = Intl.DateTimeFormat('en-IN').format(d);
+// console.log(x);
+
+// let d
+
+// d = new Date()
+
+// dnew = new Date(2023,11,25)
+// console.log(dnew)
+// console.log (Date.now())
+//!Challenge #2
+// x = Math.random()*100
+// y = Math.random()*51
+
+// let sum = Math.floor(x + y)
+// let diff = Math.floor(x - y)
+// let pro = Math.floor(x * y)
+// let div = Math.floor(x / y)
+
+// console.log(`x+y = ${sum}
+// x-y = ${diff}
+// x*y = ${pro}
+// x/y = ${div}`)
+
+// console.log(y)
+
+//!Math object
+// x = Math.random()
+
+// console.log(x)
+//! Number object
+// let x
+
+// const blah = new Number(69.1)
+
+// x = blah.toString()
+
+// x = blah.toString().length
+
+// x = blah.toFixed(10)
+
+// x = blah.toLocaleString('en-IN')
+
+// console.log(x)
+// console.log(typeof x)
+//! Exercise
+// var str = "blah"
+// array = str.charAt(0).toUpperCase()+str.slice(1)
+// // newstr = array.
+// console.log(array)
+// bleh.charAt(0) = bleh.charAt(0).toUpperCase()
 //! Some cool functions to use on strings
 // .log(s)
 
@@ -27,15 +95,15 @@
 // console.log(x)
 
 // function bleh() {
-    
+
 // }
 
-let str = "Hey There!"
-s = new String(str)
-console.log(s)
-console.log(s[9])
-xed = str.__proto__;
-console
+// let str = "Hey There!"
+// s = new String(str)
+// console.log(s)
+// console.log(s[9])
+// xed = str.__proto__;
+// console
 //!Started the 36hrs course 🫠
 // var num = 10;
 // var nem = "6969"
